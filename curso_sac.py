f7d5cb4a61f748d118a92f4568ad21908a3aed2a
Tayane Emanuely da Silva <146960458+SilvaTayane@users.noreply.github.com>
2023-12-13 09:52:59 -0400

    Update curso_sac.py

--- curso_sac.py
+++ curso_sac.py
@@ -1,18 +1,28 @@
 import streamlit as st
 
 st.set_page_config(page_title="Smart City")
-st.title("What's your problem?")
+st.title("Formulário de denúncia")
 
 
 d = {}
 with st.form("Formulário de reclamação:"):
-    a, b = st.columns(2)
-    d['nome'] = a.text_input('Nome:')
-    d['sobrenome'] = b.text_input('Sobrenome:')
+    d['nome'] = st.text_input('Nome Completo:')
+    d['telefone'] = st.text_input('Telefone: ')
 
-    d['email'] = st.text_input('E-mail: ')
-    d['endereco'] = st.text_input('Endereço')
+    option = st.selectbox(
+        'Selecione o problema',
+        ('Vazamentos', 'Esgotos a céu aberto', 'Buracos nas vias', 'Fiação elétrica', 'Lixo acumulado', 'Falta de internet', 'Poda de árvores', 'Outros'),
 
+        index=None,
+        placeholder="-------",
+    )
+    
+    d['endereco'] = st.text_input('Endereço:')
+    d['ponto_referencia'] = st.text_input('Ponto de referência:')
     d['comentário'] = st.text_area('Um comentário: ')
 
-    st.form_submit_button('Enviar')
+    button_pressed = st.form_submit_button('Enviar')
+    if button_pressed:
+        st.write("Denúncia concluída com sucesso!\n")
+
+
